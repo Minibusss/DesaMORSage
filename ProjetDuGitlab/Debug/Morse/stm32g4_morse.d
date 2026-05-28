7a7667356ab0837fc5ab6c04174c5d1bd2a9cd4c
AliceGedoux <gedouxalice@gmail.com>
2026-05-28 22:01:26 +0200

    Nettoyage : ajout .gitignore, suppression metadata et build

--- ProjetDuGitlab/Debug/Morse/stm32g4_morse.d
+++
@@ -1,2 +0,0 @@
-Morse/stm32g4_morse.o: ../Morse/stm32g4_morse.c ../Morse/stm32g4_morse.h
-../Morse/stm32g4_morse.h:
--- ProjetDuGitlab/Debug/drivers/bsp/WS2812/WS2812_asm.d
+++
@@ -1,1 +0,0 @@
-drivers/bsp/WS2812/WS2812_asm.o: ../drivers/bsp/WS2812/WS2812_asm.s
